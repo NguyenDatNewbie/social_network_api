54107fbfc251880dcee6b3eff98f18bb91d8dad2
lelong <lelong27022002@gmail.com>
2024-02-27 22:18:41 +0700

    create BaseEntity

---
+++ src/main/java/fit/api/social_network/model/entity/BaseEntity.java
@@ -0,0 +1,25 @@
+package fit.api.social_network.model.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+@Getter
+@Setter
+@MappedSuperclass
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_date")
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "modified_date")
+    private Date modifiedDate;
+    private Integer status = 1;
+}
